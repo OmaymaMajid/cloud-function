7432cdf7abcb8d7070b8548857406e191c71c294
Omayma Majid <omaymamjd05@gmail.com>
2021-05-30 00:36:19 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,26 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+/* eslint-disable eol-last */
 import * as functions from "firebase-functions";
-
+import * as admin from "firebase-admin";
+const cors = require("cors")({origin: true});
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp();
+const db = admin.firestore();
 export const helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+  functions.logger.info("Hello logs!", {structuredData: true});
+  cors(request, response, ()=>{
+    functions.logger.info("Hello logs!", {structuredData: true});
+    const docRef = db.collection("Natsu").doc("hrPVDObtrvwml9PQg5vS");
+    docRef.get().then((doc) => {
+      if (!doc.exists) {
+        console.log("No such document");
+        return response.send("Not Found");
+      }
+      console.log(doc.data());
+      return response.send(doc.data());
+    }).catch((err) => {
+      console.log("Error getting Document", err);
+    });
+  });
+});
